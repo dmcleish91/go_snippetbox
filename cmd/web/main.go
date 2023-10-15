57451e7c7555e3aec556bcef0dbe5ae1863be36b
Dwight Mcleish Jr <dmcleishjr@gmail.com>
2023-10-15 01:37:27 -0400

    added cert and https api

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -72,7 +72,7 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
